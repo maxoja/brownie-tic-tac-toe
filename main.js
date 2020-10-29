a6a09e7dd957e17c8f1011f5996bf8ea96721599
= <->
2020-10-29 15:11:51 +0000

    Add simple express api

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 4001
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/xo/health', () => {
+  res.sendStatus(200)
+})
+app.post('/xo/post-test', (req, res) => {
+  console.log('Got body:', req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => console.log(`Started server at http://localhost:${port}`));
