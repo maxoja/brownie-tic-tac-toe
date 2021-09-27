dafe866102c136f32da2827ed730e879092eeb7e
maxoja <tawan.tpptnk@gmail.com>
2021-09-27 15:18:00 +0000

    Transform try catch to async catch in main frontend script

--- frontend/script.js
+++ frontend/script.js
@@ -2,24 +2,49 @@ showPlaceButton(false)
 elems.fetchButton.style.display = 'none'
 elems.switchBtn1.style.display = 'none'
 elems.switchBtn2.style.display = 'none'
-
 // alert('Please click on a player name to pick a side')
-async function printBoard() {
-  console.log(await getGameState())
-}
 
-async function fetchAndRenderForever() {
-  while(true) {
-    try {
-      const gameState = await getGameState()
-      renderGame(gameState)
-    } catch(e) {
-      console.log(e)
+class Game {
+  breakSignal = false;
+  state = null;
+
+  async startUpdateLoopAsync() {
+    while(!this.breakSignal) {
+      let lock = true
+      
+      getGameState()
+        .then(s => {
+          this.state = s;
+          renderGame(s);
+          lock = false;
+        })
+        .catch(e => {
+          console.log(e)
+          lock = false;
+        })
+
+      while(lock)
+        await sleep(50)
+
+      // try {
+      //   this.state = await getGameState()
+      //   renderGame(this.state)
+      // } catch(e) {
+      //   console.log(e)
+      // }
+      // await sleep(200)
     }
-    // break;
-    await sleep(200)
+  }
+
+  breakGameLoop() {
+    this.breakSignal = true;
+  }
+
+  printBoard() {
+    console.log(this.state);
   }
 }
 
 testConnection();
-fetchAndRenderForever()
+const game = new Game()
+game.startUpdateLoopAsync()
